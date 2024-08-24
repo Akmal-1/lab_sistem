49a9aae1140b8de80a9acf92a0abbfed106d2d2e
Akmal-1 <arisakmal@gmail.com>
2024-08-25 03:33:51 +0700

    Memperbaiki tampilan navbar dan menambahkan logo perusahaan serta memperbaiki dropdown profil user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
 
     <!-- Menyertakan Font Awesome untuk ikon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- Menyertakan Bootstrap Icons untuk ikon -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
     <!-- Menyertakan partial untuk navbar -->
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,31 +1,46 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">My Application</a> <!-- Atau beri nama yang sesuai -->
+    <div class="container-fluid">
+        <!-- Menambahkan logo perusahaan di sebelah kiri -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/PT-Timah-Industri.svg') }}" alt="Logo PT Timah Industri" width="120">
+        </a>
 
-    <!-- Tombol toggle untuk navigasi responsif -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <!-- Tombol toggle untuk navigasi responsif -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <!-- Navigasi utama -->
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('samples.index') }}">Samples</a> <!-- Mengubah rute ke Samples -->
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('profile') }}">Profile</a>
-            </li>
-            <!-- Log Out -->
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}"
-                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    Log Out
-                </a>
-                <!-- Form logout disembunyikan -->
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        </ul>
+        <!-- Navigasi utama -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('samples.index') }}">Samples</a>
+                </li>
+            </ul>
+
+            <!-- Dropdown Profil User -->
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-person-circle" style="font-size: 30px;"></i> <!-- Ikon Bootstrap -->
+                        <span class="ms-2">{{ Auth::user()->name }}</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                        <li><a class="dropdown-item" href="{{ route('profile') }}">Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Log Out
+                            </a>
+                            <!-- Form logout disembunyikan -->
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
